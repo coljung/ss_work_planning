75550b710f1621b655e0df16f50605c92e83e25b
coljung <coljung.work@gmail.com>
2017-10-19 10:29:32 -0400

    PLANSOFT 20: budget creation

--- app/budgets/BudgetCreate.jsx
+++ app/budgets/BudgetCreate.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'antd';
+import { Modal, Select } from 'antd';
+
+const Option = Select.Option;
 
 export default class BudgetCreate extends Component {
+
+    handleChange = (value) => {
+        console.log(`selected ${value}`);
+    }
 
     render() {
         return (
             <Modal
-                title="Basic Modal"
+                title="Create Budget"
                 visible={this.props.visible}
                 onOk={this.handleOk}
                 onCancel={this.props.onOverlayClick}>
@@ -15,6 +21,14 @@ export default class BudgetCreate extends Component {
                 <p>Some contents...</p>
                 <p>Some contents...</p>
                 <p>Some contents...</p>
+                <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange}>
+                    <Option value="ss17">SS17</Option>
+                    <Option value="fw17">FW17</Option>
+                    <Option value="ss18">SS18</Option>
+                    <Option value="fw18">FW18</Option>
+                    <Option value="ss19">SS19</Option>
+                    <Option value="fw19">FW19</Option>
+                </Select>
             </Modal>
         );
     }
