4e997dc1b94824383d49ad05584b6ce299f42520
alsukurr <k.alsu@me.com>
2017-11-07 15:19:46 -0500

    UI spec change

--- app/views/top-down/exec/ExecViewContainer.jsx
+++ app/views/top-down/exec/ExecViewContainer.jsx
@@ -39,22 +39,35 @@ const cellStyle = [
 const nested = [
     [
         { label: '&nbsp;', colspan: 2, className: 'ffffff' },
-        { label: 'Women + Men', colspan: 3 },
-        { label: 'Women', colspan: 3 },
-        { label: 'Men', colspan: 3 },
+        { label: 'Women + Men', colspan: 6 },
+        { label: 'Women', colspan: 7 },
+        { label: 'Men', colspan: 7 },
     ],
     [
         'Metrics',
         'Season/Year',
-        { label: 'Before Markdowns', colspan: 1 },
+        { label: 'STD Pre-Markdown', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        { label: 'STD Post-Markdown', colspan: 1 },
         { label: 'Inc %', colspan: 1 },
         { label: 'Full Season', colspan: 1 },
-        { label: 'Before Markdowns', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        
+        { label: 'STD Pre-Markdown', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        { label: 'STD Post-Markdown', colspan: 1 },
         { label: 'Inc %', colspan: 1 },
         { label: 'Full Season', colspan: 1 },
-        { label: 'Before Markdowns', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        { label: 'Cont %', colspan: 1 },
+
+        { label: 'STD Pre-Markdown', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        { label: 'STD Post-Markdown', colspan: 1 },
         { label: 'Inc %', colspan: 1 },
         { label: 'Full Season', colspan: 1 },
+        { label: 'Inc %', colspan: 1 },
+        { label: 'Cont %', colspan: 1 },
     ],
 ];
 
