074e9f4f3a085f5b898f51126dff58c306ecfd4a
coljung <coljung.work@gmail.com>
2017-11-17 14:22:23 -0500

    PLANSFOT 151: men/women folder structure

---
+++ app/views/top-down/men/MenViewActions.js
@@ -0,0 +1,42 @@
+import agent from 'superagent';
+import wrap from 'superagent-promise';
+import getApiUrl from 'helpers';
+import { messages } from 'notifications/NotificationActions';
+
+const request = wrap(agent, Promise);
+
+export const REQUEST_BUDGETS_MEN_VIEW = 'REQUEST_BUDGETS_MEN_VIEW';
+export const RECEIVE_BUDGETS_MEN_VIEW = 'RECEIVE_BUDGETS_MEN_VIEW';
+export const RESET_BUDGETS_MEN_VIEW = 'RESET_BUDGETS_MEN_VIEW';
+
+export function requestBudgetMenViewData() {
+    return {
+        type: REQUEST_BUDGETS_MEN_VIEW,
+    };
+}
+
+function receiveBudgetMenViewData(viewData) {
+    return {
+        type: RECEIVE_BUDGETS_MEN_VIEW,
+        viewData,
+    };
+}
+
+export function resetBudgetViews() {
+    return {
+        type: RESET_BUDGETS_MEN_VIEW,
+    };
+}
+
+export function fetchBudgetMenData() {
+    return (dispatch) => {
+        dispatch(requestBudgetMenViewData());
+        return request
+            // .get(`${getApiUrl()}planning/seasons/show/available`)
+            .get('http://localhost:3001/planning/budgets/versions/13/exec')
+            .then(
+            res => dispatch(receiveBudgetMenViewData(res.body)),
+            err => dispatch(messages({ content: err, response: err.response, isError: true })),
+            );
+    };
+}
---
+++ app/views/top-down/women/WomenViewActions.js
@@ -0,0 +1,42 @@
+import agent from 'superagent';
+import wrap from 'superagent-promise';
+import getApiUrl from 'helpers';
+import { messages } from 'notifications/NotificationActions';
+
+const request = wrap(agent, Promise);
+
+export const REQUEST_BUDGETS_WOMEN_VIEW = 'REQUEST_BUDGETS_WOMEN_VIEW';
+export const RECEIVE_BUDGETS_WOMEN_VIEW = 'RECEIVE_BUDGETS_WOMEN_VIEW';
+export const RESET_BUDGETS_WOMEN_VIEW = 'RESET_BUDGETS_WOMEN_VIEW';
+
+export function requestBudgetWomenViewData() {
+    return {
+        type: REQUEST_BUDGETS_WOMEN_VIEW,
+    };
+}
+
+function receiveBudgetWomenViewData(viewData) {
+    return {
+        type: RECEIVE_BUDGETS_WOMEN_VIEW,
+        viewData,
+    };
+}
+
+export function resetBudgetViews() {
+    return {
+        type: RESET_BUDGETS_WOMEN_VIEW,
+    };
+}
+
+export function fetchBudgetWomenData() {
+    return (dispatch) => {
+        dispatch(requestBudgetWomenViewData());
+        return request
+            // .get(`${getApiUrl()}planning/seasons/show/available`)
+            .get('http://localhost:3001/planning/budgets/versions/13/exec')
+            .then(
+            res => dispatch(receiveBudgetWomenViewData(res.body)),
+            err => dispatch(messages({ content: err, response: err.response, isError: true })),
+            );
+    };
+}
