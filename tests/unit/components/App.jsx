553a979caac9443c6ecd3b778b503b64fbd07ba2
coljung <coljung.work@gmail.com>
2018-01-16 13:50:40 -0500

    PLANSOFT-329: unit tests setup

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import renderer from 'react-test-renderer';
 import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
-describe("<App />", () => {
-	let props;
-
-	beforeEach(() => {
-        props = {
-            location: { pathname: '' },
-        };
-    });
-
-	it('should render the mulitple components', () => {
-		const wrapper = shallow(<App {...props} ><div className="test" /></App>);
-		expect(wrapper.find(HeaderContent)).to.have.lengthOf(1);
-		expect(wrapper.find("main")).to.have.lengthOf(1);
-	});
-
-});
+describe('App', () => {
+	it('should render correctly', () => {
+		const tree = renderer.create(
+			<App />
+		)
+		expect(tree).toMatchSnapshot()
+	})
+})
