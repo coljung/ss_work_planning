0f512eacb2ad8883a376a80352514d89422271e9
coljung <coljung.work@gmail.com>
2018-01-18 15:19:22 -0500

    Plansoft 329 app tests

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -3,15 +3,18 @@ jest.mock('../../../app/notifications/NotificationManager', () => 'NotificationM
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
 describe('App', () => {
     it('should render correctly', () => {
-        const wrapper = shallow(
+        const testRenderer = TestRenderer.create(
             <App />
         );
-        expect(wrapper).toMatchSnapshot()
+        // const wrapper = shallow(
+        //     <App />
+        // );
+        expect(testRenderer).toMatchSnapshot()
 	});
 })
