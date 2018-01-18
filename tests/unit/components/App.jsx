0b6b890738d9ffd43df02b4c9a1120d4a7403749
coljung <coljung.work@gmail.com>
2018-01-18 15:19:22 -0500

    Plansoft 329 app tests

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
 describe('App', () => {
     it('should render correctly', () => {
-        const wrapper = shallow(
+        const testRenderer = TestRenderer.create(
             <App />
         );
-        expect(wrapper).toMatchSnapshot()
+        // const wrapper = shallow(
+        //     <App />
+        // );
+        expect(testRenderer).toMatchSnapshot()
 	});
 })
