0ef7d5a1d24b55adfe2c735e4b8a1401a2021f93
coljung <coljung.work@gmail.com>
2018-01-19 12:17:10 -0500

    PLANSOFT-329: test updates

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -1,3 +1,6 @@
+jest.mock('../../../app/components/common/NavigationMain', () => 'NavigationMain');
+jest.mock('../../../app/notifications/NotificationManager', () => 'NotificationManager12345643');
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import TestRenderer from 'react-test-renderer';
@@ -7,11 +10,11 @@ import HeaderContent from '../../../app/components/common/HeaderContent';
 describe('App', () => {
     it('should render correctly', () => {
         const testRenderer = TestRenderer.create(
-            <App />
+            <App  location={{pathname: 'pathname'}} />
         );
-        // const wrapper = shallow(
-        //     <App />
-        // );
+        const wrapper = shallow(
+            <App  location={{pathname: 'pathname'}} />
+        );
         expect(testRenderer).toMatchSnapshot()
 	});
 })
