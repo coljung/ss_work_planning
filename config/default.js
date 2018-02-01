03ba72bb1179f22531bbc6edf330da31660db132
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-01 13:48:30 -0500

    Add BudgetAction test - redux

--- config/default.js
+++ config/default.js
@@ -2,7 +2,8 @@ module.exports = {
     server: {
         host: process.env.HTTP_UI_HOST || '0.0.0.0',
         port: process.env.HTTP_UI_PORT || 8007,
-        exposedHost: process.env.UI_PLANNING_HOST || 'localhost',
+        // exposedHost: process.env.UI_PLANNING_HOST || 'localhost',
+        exposedHost: process.env.UI_PLANNING_HOST || 'http://127.0.0.1',
         exposedPort: process.env.UI_PLANNING_PORT || 8007
     },
     auth: {
