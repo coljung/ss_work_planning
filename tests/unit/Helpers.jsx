d1ee8e8eca13aa93c53b58a74e047f81faf91689
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-02 11:43:00 -0500

    use the renamed defaultMetricSequence function

--- tests/unit/Helpers.jsx
+++ tests/unit/Helpers.jsx
@@ -1,11 +1,11 @@
-import { defaultMetricString } from '../../app/Helpers';
+import { defaultMetricSequence } from '../../app/Helpers';
 import getApiUrl from '../../app/Helpers';
 
 describe('Helpers functions', () => {
   it('should get the API URL', () => expect(getApiUrl()).toEqual('http://127.0.0.1/api/'));
 
   it('should get default metric query string', () => {
-    expect(defaultMetricString()).toEqual(
+    expect(defaultMetricSequence()).toEqual(
       'iGM%,iRETAIL,BOM COST,RECEIPT%,RECEIVED COST,GM%,GM$,COGS,SALES'
     );
   });
