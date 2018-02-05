9f8f6c8297e1be29e3d3976f6091a3aaa9a1f84b
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-05 10:12:39 -0500

    Rename App render test var

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -3,18 +3,15 @@ jest.mock('../../../app/notifications/NotificationManager', () => 'NotificationM
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import TestRenderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
 describe('App', () => {
     it('should render correctly', () => {
-        const testRenderer = TestRenderer.create(
+        const app = renderer.create(
             <App  location={{pathname: 'pathname'}} />
         );
-        const wrapper = shallow(
-            <App  location={{pathname: 'pathname'}} />
-        );
-        expect(testRenderer).toMatchSnapshot()
+        expect(app).toMatchSnapshot()
 	});
 })
