eeac9dc2a303447cc61df261c384d4aecc3d44b5
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-06 13:19:46 -0500

    PLANSOFT-329: unit tests setup

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -1,20 +1,14 @@
-jest.mock('../../../app/components/common/NavigationMain', () => 'NavigationMain');
-jest.mock('../../../app/notifications/NotificationManager', () => 'NotificationManager12345643');
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import TestRenderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
 describe('App', () => {
-    it('should render correctly', () => {
-        const testRenderer = TestRenderer.create(
-            <App />
-        );
-        // const wrapper = shallow(
-        //     <App />
-        // );
-        expect(testRenderer).toMatchSnapshot()
-	});
+	it('should render correctly', () => {
+		const tree = renderer.create(
+			<App />
+		)
+		expect(tree).toMatchSnapshot()
+	})
 })
