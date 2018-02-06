fe161409ee4b65d29269ddf00d1f5285c421e77a
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-06 13:12:23 -0500

    Fix merge conflict

--- tests/unit/components/App.jsx
+++ tests/unit/components/App.jsx
@@ -8,10 +8,10 @@ import App from '../../../app/components/App';
 import HeaderContent from '../../../app/components/common/HeaderContent';
 
 describe('App', () => {
-	it('should render correctly', () => {
-		const tree = renderer.create(
-			<App />
-		)
-		expect(tree).toMatchSnapshot()
-	})
+    it('should render correctly', () => {
+        const wrapper = shallow(
+            <App />
+        );
+        expect(wrapper).toMatchSnapshot()
+	});
 })
