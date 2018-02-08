31ddc8523dc7307d526b6fd7be301049663dd212
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-08 07:56:25 -0500

    HeaderContent performance optimizations - make it stateless componenet

--- app/components/common/HeaderContent.jsx
+++ app/components/common/HeaderContent.jsx
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from './Logo';
 import NavigationUser from './NavigationUser';
 
-
-export default class HeaderContent extends Component {
-    render() {
-        return (
-            <div className="headerContent">
-                <Logo />
-                <h1>Merchandise Planning Tool</h1>
-                <NavigationUser />
-            </div>
-        );
-    }
-}
+export default () =>
+  <div className="headerContent">
+     <Logo />
+     <h1>Merchandise Planning Tool</h1>
+     <NavigationUser />
+  </div>
