26d56fe6a1ec1ea8c546d19ed2da43dedc46b3b5
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-02-08 08:22:38 -0500

    Fix Logo and test to stateless component

--- tests/unit/components/common/Logo.jsx
+++ tests/unit/components/common/Logo.jsx
@@ -3,7 +3,9 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import Logo from '../../../../app/components/common/Logo';
 
-describe.skip('<Logo />', () => {
+let props;
+
+describe('<Logo />', () => {
     it('should render base layout', () => {
         const wrapper = shallow(<Logo />);
         expect(wrapper.find('svg')).to.have.lengthOf(1);
