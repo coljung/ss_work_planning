27a89f7e15d0eb5d25b304c841acfb8235dd508e
coljung <coljung.work@gmail.com>
2018-03-15 14:51:36 -0400

    PLANSOFT 416: unit tests updates for reducers

--- app/views/top-down/common/viewReducers.js
+++ app/views/top-down/common/viewReducers.js
@@ -10,12 +10,12 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case RESET_BUDGETS_VIEW:
-            return initialState;
         case REQUEST_BUDGETS_VIEW:
             return initialState;
         case RECEIVE_BUDGETS_VIEW: {
             const setData = [];
             setData[action.view] = action.viewData;
+            console.log(setData);
             return Object.assign({}, state, {
                 viewData: setData,
                 viewDataFetched: true,
