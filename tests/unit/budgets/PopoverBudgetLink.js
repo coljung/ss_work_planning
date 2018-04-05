051f163d3349a09edb51fe2f2f06018dd82b48bf
coljung <coljung.work@gmail.com>
2018-04-05 16:14:42 -0400

    PLANSOFT 552: default test

--- tests/unit/budgets/PopoverBudgetLink.js
+++ tests/unit/budgets/PopoverBudgetLink.js
@@ -7,17 +7,8 @@ import PopoverBudgetLink from '../../../app/budgets/PopoverBudgetLink';
 describe('PopoverBudgetLink', () => {
     it('should render correctly', () => {
         const output = shallow(
-            <PopoverBudgetLink title="fsdfsdf">Hello Jest!</PopoverBudgetLink>
+            <PopoverBudgetLink>Links here!</PopoverBudgetLink>
         );
-        expect(shallowToJson(output)).toMatchSnapshot();
-    });
-
-    it.skip('should render correctly with btnInTitle', () => {
-        const output = shallow(
-            <Board title="Hello World!" btnInTitle={<Button>Foo Bar</Button>}>Hello Jest!</Board>
-        );
-
-        expect(output.find('Button').render().text()).toBe('Foo Bar');
         expect(shallowToJson(output)).toMatchSnapshot();
     });
 });
