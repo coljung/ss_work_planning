1f898c2da3ddaa65dd2e642e74a803322e9aaafb
coljung <coljung.work@gmail.com>
2018-04-06 14:52:48 -0400

    PLANSOFT-478: linting updates

--- app/views/top-down/common/ViewCommonContainer.jsx
+++ app/views/top-down/common/ViewCommonContainer.jsx
@@ -198,6 +198,7 @@ ViewCommonContainer.propTypes = {
     resetState: PropTypes.func.isRequired,
     budget: PropTypes.string.isRequired,
     version: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
     view: PropTypes.string.isRequired,
     router: PropTypes.object.isRequired,
     cellRendererFactory: PropTypes.func.isRequired,
