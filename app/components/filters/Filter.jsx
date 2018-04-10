c817da06c098e6ba5d881c12bb82e71afb6e73bd
coljung <coljung.work@gmail.com>
2018-04-10 11:42:18 -0400

    PLANSOFT-525: filter json updated

--- app/components/filters/Filter.jsx
+++ app/components/filters/Filter.jsx
@@ -2,10 +2,181 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Button, Modal, Spin, Checkbox, Row, Col  } from 'antd';
-import { resetState } from "../../budgets/BudgetActions";
+import { Button, Modal, List, Checkbox, Row, Col } from 'antd';
+import { resetState } from '../../budgets/BudgetActions';
 
 const CheckboxGroup = Checkbox.Group;
+
+const initialData = [
+    {
+        metrics: {
+            sales: true,
+            cogs: false,
+            gm_percentage: true,
+            gm_dollar: false,
+        },
+        seasons: {
+            sales: {
+                ss19: true,
+                ss18: true,
+                ss16: true,
+                ss15: true,
+                ss14: true,
+            },
+            cogs: {
+                ss19: true,
+                ss18: true,
+                ss16: true,
+                ss15: true,
+                ss14: true,
+            },
+            gm_percentage: {
+                ss19: true,
+                ss18: true,
+                ss16: true,
+                ss15: true,
+                ss14: true,
+            },
+            gm_dollar: {
+                ss19: true,
+                ss18: true,
+                ss16: true,
+                ss15: true,
+                ss14: true,
+            },
+        },
+        options: {
+            sales: {
+                ss19: {
+                    WP: true,
+                    OP: true,
+                    ACT: true,
+                    PRJ: true,
+                },
+                ss18: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss16: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss15: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss14: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+            },
+            cogs: {
+                ss19: {
+                    WP: true,
+                    OP: true,
+                    ACT: true,
+                    PRJ: true,
+                },
+                ss18: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss16: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss15: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss14: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+            },
+            gm_percentage: {
+                ss19: {
+                    WP: true,
+                    OP: true,
+                    ACT: true,
+                    PRJ: true,
+                },
+                ss18: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss16: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss15: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss14: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+            },
+            gm_dollar: {
+                ss19: {
+                    WP: true,
+                    OP: true,
+                    ACT: true,
+                    PRJ: true,
+                },
+                ss18: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss16: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss15: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+                ss14: {
+                    WP: true,
+                    OP: false,
+                    ACT: false,
+                    PRJ: false,
+                },
+            },
+        },
+    },
+];
 
 export class Filter extends Component {
 
@@ -15,20 +186,16 @@ export class Filter extends Component {
             year: '',
             season: '',
             metric: '',
+            dataMetrics: initialData[0].metrics,
+            dataSeason: initialData[0].seasons,
+            dataOptions: initialData[0].options,
         };
     }
 
     componentWillReceiveProps(props) {
-        if (props.visible && !props.seasonsFetched) {
-        }
+        if (props.visible && !props.seasonsFetched) {}
     }
 
-    onChange = (e) => {
-        this.setState({
-            createNewValue: e.target.value,
-        });
-    }
-    
     filterView = () => {
         const filter = {
             year: this.state.year,
@@ -55,8 +222,16 @@ export class Filter extends Component {
         );
     }
 
+    onChange = (checkedValues) => {
+        // console.log('checked = ', checkedValues);
+    }
+
+    viewNext = () => {
+
+    }
+
     render() {
-        console.log(this);
+        // console.log(this.state.dataMetrics);
         const footerButtons = (<div>
             <Button
                 onClick={this.filterView.bind(this)}
@@ -72,59 +247,100 @@ export class Filter extends Component {
         </div>);
 
         // const modalContent = this.createModalContent;
-        
+
         const filterMetrics = [
-            {label: 'sales', value: 'sales'},
-            {label: 'cogs', value: 'cogs'},
-            {label: 'gm$', value: 'gm$'},
-            {label: 'gm%', value: 'gm%'},
+            { label: 'sales', value: 'sales' },
+            { label: 'cogs', value: 'cogs' },
+            { label: 'gm$', value: 'gm_dollar' },
+            { label: 'gm%', value: 'gm_percentage' },
         ];
 
         const filterSeasons = [
-            {label: 'ss19', value: 'ss19'},
-            {label: 'ss18', value: 'ss18'},
-            {label: 'ss17', value: 'ss17'},
-            {label: 'ss16', value: 'ss16'},
+            { label: 'ss19', value: 'ss19' },
+            { label: 'ss18', value: 'ss18' },
+            { label: 'ss17', value: 'ss17' },
+            { label: 'ss16', value: 'ss16' },
         ];
         const filterOptions = [
-            {label: 'WP', value: 'WP'},
-            {label: 'OP', value: 'OP'},
-            {label: 'ACT', value: 'ACT'},
-            {label: 'PRJ', value: 'PRJ'},
+            { label: 'WP', value: 'WP' },
+            { label: 'OP', value: 'OP' },
+            { label: 'ACT', value: 'ACT' },
+            { label: 'PRJ', value: 'PRJ' },
         ];
 
-        const metrics = { 'sales': true, 'cogs': false };
-        const seasons = { 'sales': { 'ss19': true, 'ss18': true, 'ss17': true, 'ss16': true, 'ss15': true }, 'cogs': { 'ss19': true, 'ss18': false, 'ss17': true, 'ss16': true, 'ss15': true } };
-        const options = { 'ss19': { 'WP': true, 'OP': false } };
+        const displayLinkFunctionMetrics = (test) => {
+            // console.log(this.state.dataMetrics);
+            // console.log(test.value);
+            // console.log(typeof this.state.dataMetrics['sales']);
+            // console.log(typeof this.state.dataMetrics[test.value]);
+            return (<div>
+                <Checkbox value={test.value} defaultChecked={true}>{test.label}</Checkbox>
+                <Button onClick={this.viewNext}>view</Button>
+            </div>);
+        };
 
-        console.log(seasons.cogs.ss18);
+        const displayLinkFunction = test =>
+            <div>
+                <Checkbox value={test.value}>{test.label}</Checkbox>
+                <Button onClick={this.viewNext}>view</Button>
+            </div>;
+
+        const metrics = filterMetrics.map(displayLinkFunctionMetrics);
+        const seasons = filterSeasons.map(displayLinkFunction);
+        const options = filterOptions.map(displayLinkFunction);
+        //debugger;
 
 
-        const modalContent = <Row>
-            <Col span={8}>
-              <h2>Metrics</h2>
-              {/* <CheckboxGroup options={filterMetrics} defaultValue={["metric1"]} /> */}
-              <ul>
-              </ul>
-            </Col>
-            <Col span={8}>
-              <h2>Seasons</h2>
-              <CheckboxGroup options={filterSeasons} defaultValue={["season2"]} />
-            </Col>
-            <Col span={8}>
-              <h2>Row</h2>
-              <CheckboxGroup options={filterOptions} defaultValue={["row3"]} />
-            </Col>
-          </Row>;
+        const modalContent = (
+            <div>
+                <Row>
+                    <Col span={8}>
+                        <CheckboxGroup defaultValue={['sales']} style={{ width: '100%' }} onChange={this.onChange}>
+                            <List
+                                size="small"
+                                header={null}
+                                footer={null}
+                                bordered
+                                dataSource={metrics}
+                                renderItem={item => (<List.Item>{item}</List.Item>)}
+                                />
+                        </CheckboxGroup>
+                    </Col>
+                    <Col span={8}>
+                        <CheckboxGroup style={{ width: '100%' }} onChange={this.onChange}>
+                            <List
+                                size="small"
+                                header={null}
+                                footer={null}
+                                bordered
+                                dataSource={seasons}
+                                renderItem={item => (<List.Item>{item}</List.Item>)}
+                                />
+                        </CheckboxGroup>
+                    </Col>
+                    <Col span={8}>
+                        <CheckboxGroup style={{ width: '100%' }} onChange={this.onChange}>
+                            <List
+                                size="small"
+                                header={null}
+                                footer={null}
+                                bordered
+                                dataSource={options}
+                                renderItem={item => (<List.Item>{item}</List.Item>)}
+                                />
+                        </CheckboxGroup>
+                    </Col>
+                </Row>
+            </div>
 
-        <Spin size="large" />;
+        );
 
         return (
             <Modal
-                title="Filter"
-                visible={this.props.visible}
+                title='Filter'
+                visible={true}
                 onOk={this.handleOk}
-                width={400}
+                width={600}
                 onCancel={this.closeModal}
                 footer={footerButtons}>
                 {modalContent}
