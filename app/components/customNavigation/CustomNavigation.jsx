bdcd20eb4d00d465a0eb2298f89fc0e4a7605445
TetyanaMyronova <tetyana.myronova@ssense.com>
2018-06-19 13:51:16 -0400

    chore/PLANSOFT-847: detect when menu is open then re-render the page

--- app/components/customNavigation/CustomNavigation.jsx
+++ app/components/customNavigation/CustomNavigation.jsx
@@ -23,6 +23,7 @@ class CustomNavigation extends Component {
 
     handleClick(e) {
         this.setState({ current: e.key });
+        this.props.customOnClick();
     }
 
     render() {
@@ -169,6 +170,7 @@ CustomNavigation.propTypes = {
     versionId: PropTypes.string,
     seasonName: PropTypes.string,
     versionName: PropTypes.string,
+    customOnClick: PropTypes.func,
 };
 
 function mapStateToProps(state) {
