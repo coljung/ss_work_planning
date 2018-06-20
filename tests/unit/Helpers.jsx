fd91295f0f89f8697127187816dadb857ca7ac8f
coljung <coljung.work@gmail.com>
2018-06-20 13:16:35 -0400

    Unit tests updaytes

--- tests/unit/Helpers.jsx
+++ tests/unit/Helpers.jsx
@@ -3,10 +3,4 @@ import getApiUrl from '../../app/Helpers';
 
 describe('Helpers functions', () => {
   it('should get the API URL', () => expect(getApiUrl()).toEqual('http://127.0.0.1/api/'));
-
-  it('should get default metric query string', () => {
-    expect(defaultMetricSequence()).toEqual(
-      'SALES,COGS,GM$,GM%,RECEIVED COST,RECEIPT%,BOM COST,iRETAIL,iGM%,TURNOVER RATE'
-    );
-  });
 });
