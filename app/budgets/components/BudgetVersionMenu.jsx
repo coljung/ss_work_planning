ebe514818741697668be0dcddb8756860aaad10b
Sahadat Hussein <sahadat.hussein@ssense.com>
2018-07-13 10:01:03 -0400

    fix lint error

--- app/budgets/components/BudgetVersionMenu.jsx
+++ app/budgets/components/BudgetVersionMenu.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BudgetVersionMenu = ({currentSeason, currentVersion }) => {
-    return (
-        <div>
+const BudgetVersionMenu = ({ currentSeason, currentVersion }) => (
+     <div>
             <h3> {currentSeason} - {currentVersion} </h3>
-        </div>
-    );
-};
+     </div>
+);
 
 BudgetVersionMenu.propTypes = {
     currentSeason: PropTypes.string,
