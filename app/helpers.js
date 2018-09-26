7ee4fb7f8340b57eea9ce5cd6e525a5dee456a3b
Mathieu Doyon <mathieu.doyon@ssense.com>
2018-09-26 16:20:33 -0400

    Add tests for users auth

--- app/helpers.js
+++ app/helpers.js
@@ -3,6 +3,6 @@ export default function getApiUrl(withApi = true) {
     // toogle prefixing path with `/api`
     // eslint-disable-next-line no-undef
     return process.env.NODE_ENV === 'test' ?
-        withApi ? `${UI_PLANNING_HOST}/api` : `${UI_PLANNING_HOST}/` :
+        withApi ? `${UI_PLANNING_HOST}/api` : `${UI_PLANNING_HOST}` :
         withApi ? '/api' : '';
 }
